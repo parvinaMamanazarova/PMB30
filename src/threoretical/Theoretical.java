aaca6d73b006b8adb223411f18d275344ad5d0b2
parvina18 <parvinamamanazarova2@gmail.com>
2024-05-14 13:50:51 +0500

    theoretical

---
+++ src/threoretical/Theoretical.java
@@ -0,0 +1,39 @@
+package threoretical;
+
+public class Theoretical {
+    /////////////////////  ArrayList va LinkedList farqlari //////////////////////////////
+
+  /*
+   ArrayListni ichki strukturasi DateStrakchindi bir bolimi , bir qismi xisoblanadi .
+   ArrayList bekraund malumot saqlash uchun Dynamic arraydan foydalanadi.
+   ArrayList arrayda malumotlarni ketma ket joy oladi.
+
+    LinkedList Node degan narsa saqlaydi .Node bu bitta class , u ozidan kiyin qaysi element qayerda turganini biladi .
+
+    LinkedList va ArrayList --> ArrayList malumotlarni insertion orderda saqlidi , LinkedList esa Node degan class ichida saqlidi.
+
+    ////////////////////  LinkdeHashMap //////////////////////////////////////////////////
+
+    LinkedHashMap --> u xam malumotlarni DateStrakchinda saqlidi HashMap kabi , u (Array)Node tipida saqlidi.
+
+    /////////////////// Generic ///////////////////////////////////////////////////////////
+
+    Genery --> bu mavhum tip yoki parametayzet type , biz unga nima bersak o'shanga qarab ozgaraveradi.
+    Generyc --> faqargina compile time da kompilyatsiya bolaydigon typeSeftini beradi.
+    TypeSefti --> biz yozayotgan methodlarimizni faqatgina belgilangan methodlarinigina ishlata olishi bolmasa ishlata olmaydi.
+    Generyc --> bizni kodimizni yozishga osonlashtirib beradi , biz unga nima type ekanligini ayitib otirmaymiz ozi bilib oladi.
+
+    ///////////////// Collection ///////////////////////////////////////////////////////////
+
+    Collection -->      Iterabl
+            Collection
+    List                    Queue                    Set
+
+    ArrayList              PrioriteQueue             HashSet
+    LinkedList               Dequeue                 LinkedHashset
+    Vektor
+
+                                                       SortedSet
+                                                       TreeSet
+   */
+}
